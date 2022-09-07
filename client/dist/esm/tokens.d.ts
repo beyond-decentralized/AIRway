da556544f12c54060e45a642acc22c92d59c7ef4
artem-v-shamsutdinov <artem-v-shamsutdinov@dataindependence.net>
2022-09-07 10:15:47 -0600

    removed generated files

--- client/dist/esm/tokens.d.ts
+++
@@ -1,3 +0,0 @@
-import { IClient } from './Client';
-export declare const CLIENT: import("@airport/direction-indicator").IDependencyInjectionToken<IClient>;
-//# sourceMappingURL=tokens.d.ts.map
--- processor-common/dist/esm/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './BasicServer';
-export * from './server-types';
-//# sourceMappingURL=index.d.ts.map
